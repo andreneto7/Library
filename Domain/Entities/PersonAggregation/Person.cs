beafc8fd4e92eb81f39a7133b0694172a3b70ac7
Andre Ferreira de Santana Neto <andre.neto@iatec.com>
2022-07-06 10:00:22 -0300

    Atualização da regra para a solicitação de reserva da pessoa.

--- Domain/Entities/PersonAggregation/Person.cs
+++ Domain/Entities/PersonAggregation/Person.cs
@@ -13,6 +13,7 @@ namespace Domain.Entities.PersonAggregation
     /// 1 - Deve ser informado no cadastro inicial do cliente, nome, sobrenome, email e genero
     /// 2 - Todo cliente que for cadastrado, deve estar com o status inativo
     /// 3 - Nome e sobrenome serão obrigatórios
+    /// 4 - A pessoa só poderá realizar uma reserva se possuir um endereco 
     /// </summary>
     public class Person
     {
